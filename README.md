01f9d8d6836f1b20fec6f1ca4cbb9d51a2e49217
himantyu-yuma <lipton.happy.milktea@gmail.com>
2022-03-10 21:23:52 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # git-sample2022
 勉強会用のリポジトリ
+# クローラーもどき
+
+## 概要
+Playwrightを使ったクローラーもどきです。
+
+## 必要要件
+- Python3.7以上
+
+## 環境構築
+1. 仮想環境構築
+```[bash]
+python -m venv [venv-name]
+```
+1. パッケージの導入
+```[bash]
+pip install -r requirements.txt
+```
+1. Playwrightのインストール
+```[bash]
+playwright install
+playwright install-deps
+```
+
+## 実行方法
+```
+python crawl.py
+```
---
+++ crawl.py
@@ -0,0 +1,15 @@
+import pathlib
+from playwright.sync_api import sync_playwright
+
+current_dir = pathlib.Path(__file__).resolve().parent
+
+save_dir = current_dir / "html"
+with sync_playwright() as p:
+    browser = p.webkit.launch()
+    page = browser.new_page()
+    page.goto("https://ssp.kaigiroku.net/tenant/moka/SpTop.html")
+    page.screenshot(path="example.png")
+    html = page.content()
+    with open(save_dir / "test.html", mode="w", encoding="utf-8") as f:
+        f.write(html)
+    browser.close()
