eb4211ed99904d7e883f371950b11f322030b5a5
himantyu-yuma <lipton.happy.milktea@gmail.com>
2022-03-11 16:33:57 +0900

    小樽市のURLに変更

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ save_dir = current_dir / "html"
 with sync_playwright() as p:
     browser = p.webkit.launch()
     page = browser.new_page()
-    page.goto("https://ssp.kaigiroku.net/tenant/moka/SpTop.html")
+    page.goto("https://www.city.otaru.lg.jp/docs/2020113000634/")
     page.screenshot(path="example.png")
     html = page.content()
     with open(save_dir / "test.html", mode="w", encoding="utf-8") as f:
